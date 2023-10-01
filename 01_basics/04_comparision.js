a9b110bccf48af7d5b992c42ff24372bf79c1fe6
Bhargav Bandi <84260649+Bhargav110501@users.noreply.github.com>
2023-10-01 09:50:39 +0000

    Comparision operation

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,26 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+// console.log(2 > 1); //true
+// console.log(2 >= 1); //true
+// console.log(2 < 1); //false
+// console.log(2 == 1); //false
+// console.log(2 != 1); //true
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+/*
+The reason is that an quality check == and comparisons > < >= <= work differently.
+Comparions convert null to a number, treating it as 0.
+That's why (3) null >= 0 is true and (1) null > 0 is false.
+*/
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0); //false
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2); //false
