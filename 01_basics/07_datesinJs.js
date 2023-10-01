6f13604c6ff76e859b29cc5cbb8bdcbea479344a
Bhargav Bandi <84260649+Bhargav110501@users.noreply.github.com>
2023-10-01 18:02:39 +0000

    dates in js

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -1,27 +1,31 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(myDate); //2023-10-01T17:41:31.582Z
+// console.log(myDate.toString()); //Sun Oct 01 2023 17:40:50 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toDateString()); //Sun Oct 01 2023
+// console.log(myDate.toLocaleString()); //10/1/2023, 5:42:14 PM
+// console.log(typeof myDate); //object
 
 // let myCreatedDate = new Date(2023, 0, 23)
+//console.log(myCreatedDate.toLocaleString());//  1/23/2023, 12:00:00 AM
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// console.log(myCreatedDate.toLocaleString());//  1/23/2023, 5:03:00 AM
 // let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());//  1/14/2023, 12:00:00 AM
 let myCreatedDate = new Date("01-14-2023")
-// console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.toLocaleString());//  1/14/2023, 12:00:00 AM
 
 let myTimeStamp = Date.now()
 
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+// console.log(myTimeStamp);//1696182328647
+// console.log(myCreatedDate.getTime());//1673654400000
+// console.log(Math.floor(Date.now()/1000));//1696182363
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+console.log(newDate);//2023-10-01T17:46:03.637Z
+console.log(newDate.getMonth() + 1);//10
+console.log(newDate.getDay());//0
 
 // `${newDate.getDay()} and the time `
 
