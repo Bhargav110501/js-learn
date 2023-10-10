781c39210bd050e80e8ed57ae2b56a7b5c39b637
Bhargav Bandi <84260649+Bhargav110501@users.noreply.github.com>
2023-10-10 17:36:28 +0000

    objects part 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,33 +1,36 @@
 // const tinderUser = new Object()
 const tinderUser = {}
 
+// console.log(tinderUser); // {}
+
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
 tinderUser.isLoggedIn = false
 
-// console.log(tinderUser);
+// console.log(tinderUser); // { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
 const regularUser = {
     email: "some@gmail.com",
     fullname: {
         userfullname: {
-            firstname: "hitesh",
-            lastname: "choudhary"
+            firstname: "Bhargav",
+            lastname: "Bandi"
         }
     }
 }
 
-// console.log(regularUser.fullname.userfullname.firstname);
+// console.log(regularUser.fullname.userfullname.firstname);// Bhargav
 
 const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
+// console.log(obj3); // { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
-
-const obj3 = {...obj1, ...obj2}
-// console.log(obj3);
+// console.log((obj3)); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3); // { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
 
 
 const users = [
@@ -46,30 +49,31 @@ const users = [
 ]
 
 users[1].email
-// console.log(tinderUser);
+// console.log(tinderUser);// { id: '123abc', name: 'Sammy', isLoggedIn: false }
 
-// console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser));
-// console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser)); // [ 'id', 'name', 'isLoggedIn' ]
+// console.log(Object.values(tinderUser)); // [ '123abc', 'Sammy', false ]
+// console.log(Object.entries(tinderUser)); // [ [ 'id', '123abc' ], [ 'name', 'Sammy' ], [ 'isLoggedIn', false ] ]
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); // true
 
 
 const course = {
     coursename: "js in hindi",
     price: "999",
-    courseInstructor: "hitesh"
+    courseInstructor: "Bhargav"
 }
 
 // course.courseInstructor
 
-const {courseInstructor: instructor} = course
+// const {courseInstructor} = course
+// console.log(courseInstructor); // Bhargav
 
-// console.log(courseInstructor);
-console.log(instructor);
+// const {courseInstructor:instructor} = course
+// console.log(instructor); // Bhargav
 
 // {
-//     "name": "hitesh",
+//     "name": "Bhargav",
 //     "coursename": "js in hindi",
 //     "price": "free"
 // }
