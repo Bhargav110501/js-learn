2f342a371513055491c9192127bc9685b6c00467
Bhargav Bandi <84260649+Bhargav110501@users.noreply.github.com>
2023-10-10 16:50:28 +0000

    objects part 1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -7,32 +7,46 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "Bhargav",
+    "full name": "Bhargav Bandi",
     [mySym]: "mykey1",
-    age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    age: 22,
+    location: "Vizianagaram",
+    email: "bhargav@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser.email) //bhargav@google.com
+// console.log(JsUser["email"]) //bhargav@google.com
+// console.log(JsUser["full name"]) //Bhargav Bandi
+// console.log(JsUser[mySym]) // mykey1
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "bhargav@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "bhargav@microsoft.com"
 // console.log(JsUser);
+
+/* OP
+{
+  name: 'Bhargav',
+  'full name': 'Bhargav Bandi',
+  age: 22,
+  location: 'Vizianagaram',
+  email: 'bhargav@chatgpt.com',
+  isLoggedIn: false,
+  lastLoginDays: [ 'Monday', 'Saturday' ],
+  [Symbol(key1)]: 'mykey1'
+}
+*/
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
 }
+// console.log(JsUser.greeting); // [Function (anonymous)]
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`); //String Interpolation(``)
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+console.log(JsUser.greeting()); //Hello JS user
+console.log(JsUser.greetingTwo()); //Hello JS user, Bhargav
