d4c12197fce7158846111c0a1bcefa67d3cf06eb
Bhargav Bandi <84260649+Bhargav110501@users.noreply.github.com>
2023-10-13 04:02:18 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,70 @@
+// Control flow
+
+// if
+const isUserLoggedIn = true
+
+
+// if (2 == "2") {
+//     console.log("executed"); //executed
+// }
+
+// <, >, <=, >=, ==(compare vales), !=, ===(compare vales with types),
+// !==
+
+// const temperature = 41
+// if (temperature === 41) {
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("execute");
+/*
+less than 50
+execute*/
+
+// const score = 200
+// if ( score > 100 ) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);// User power: fly
+// }
+// console.log(`User power: ${power}`);
+// output will get based on block scope
+
+const balance = 1000
+// implicit scope
+// if (balance > 500) console.log("test"), console.log("test2");
+// Bad practise
+/*
+// test
+// test2
+// */
+
+// nested if else
+
+// if (balance < 500) {
+//     console.log("less than");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else if (balance < 900){
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");//less than 1200
+// }
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if ( userLoggedIn && debitCard ) {
+//     console.log("Allow to buy courses"); //Allow to buy courses
+// }
+
+// if ( loggedInFromGoogle || loggedInFromEmail) {
+//     console.log("User logged in"); // User logged in
+// }
+
+
+
