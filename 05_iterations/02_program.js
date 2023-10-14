bc803531b5884c0235c2dd198515c0305bbb5976
Bhargav Bandi <84260649+Bhargav110501@users.noreply.github.com>
2023-10-14 06:40:26 +0000

    while and do-while loop

---
+++ 05_iterations/02_program.js
@@ -0,0 +1,57 @@
+// While loop
+
+let index = 0
+while( index <= 10){
+    // console.log(`Value of index is ${index}`);
+    index += 2
+}
+/*
+Value of index is 0
+Value of index is 2
+Value of index is 4
+Value of index is 6
+Value of index is 8
+Value of index is 10
+*/
+
+let myArray = ["falsh", "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length){
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+/*
+Value is falsh
+Value is batman
+Value is superman
+*/
+
+// do - while
+
+let score = 1
+do{
+    // console.log(`score is ${score}`);
+    score++
+}while(score <= 10)
+/*
+score is 1
+score is 2
+score is 3
+score is 4
+score is 5
+score is 6
+score is 7
+score is 8
+score is 9
+score is 10
+*/
+
+// Special case
+
+
+let score1 = 11
+do{
+    // console.log(`score is ${score1}`); // score is 11
+    score1++
+}while(score1 <= 10)
