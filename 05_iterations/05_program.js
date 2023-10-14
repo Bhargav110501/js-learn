5b97a85ea6ae0abbb5839a1a2e93eab72c7e8dbb
Bhargav Bandi <84260649+Bhargav110501@users.noreply.github.com>
2023-10-14 07:52:32 +0000

    looping in objects

---
+++ 05_iterations/05_program.js
@@ -0,0 +1,84 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// callbackfn
+// coding.forEach( function (val) {
+//     console.log(val);
+// } )
+/*
+js
+ruby
+java
+python
+cpp
+*/ 
+
+// coding.forEach( (item) => {
+//     // console.log(item);
+// } )
+/*
+js
+ruby
+java
+python
+cpp
+*/
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+/*
+js
+ruby
+java
+python
+cpp
+*/ 
+
+coding.forEach( (item, index, arr)=> {
+    // console.log(item, index, arr);
+})
+
+/*
+js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+*/ 
+
+const myCoding = [
+    {
+        languageNme: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageNme: "java",
+        languageFileName: "java"
+    },
+    {
+        languageNme: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach( (item)=> {
+    // console.log(item);
+} )
+/*
+{ languageNme: 'javascript', languageFileName: 'js' }
+{ languageNme: 'java', languageFileName: 'java' }
+{ languageNme: 'python', languageFileName: 'py' }
+*/ 
+myCoding.forEach( (item)=> {
+    console.log(item.languageNme);
+} )
+/*
+javascript
+java
+pythons
+*/
+
+
+
